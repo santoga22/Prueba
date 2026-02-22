39349efe8ef4e82968b4a692196215d284ca99e8
santoga22 <santigasa22@gmail.com>
2026-02-21 18:38:29 -0600

    Updated

--- src/pruebagithub/PruebaGithub.java
+++ src/pruebagithub/PruebaGithub.java
@@ -14,7 +14,7 @@ public class PruebaGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Hola
     }
     
 }
